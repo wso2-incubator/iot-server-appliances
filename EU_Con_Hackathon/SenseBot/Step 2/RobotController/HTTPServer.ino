a1b4472641fda9d29fda5d314fff913cac2cde5f
Shabir Mohamed <shabir_tck09@hotmail.com>
2015-06-01 22:43:50 +0530

    Update HTTPServer.ino

--- EU_Con_Hackathon/SenseBot/Step 2/RobotController/HTTPServer.ino
+++ EU_Con_Hackathon/SenseBot/Step 2/RobotController/HTTPServer.ino
@@ -1,9 +1,7 @@
 
-#define WLAN_SSID       "AndroidAP"           // cannot be longer than 32 characters!
-#define WLAN_PASS       "1234567890"
+#define WLAN_SSID       "YourAccessPointSSID"           // cannot be longer than 32 characters!
+#define WLAN_PASS       "APPassword"
 
-//#define WLAN_SSID       "WSO2-Restricted"           // cannot be longer than 32 characters!
-//#define WLAN_PASS       "LKvene8xIOT"
 // Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
 #define WLAN_SECURITY   WLAN_SEC_WPA2
 
