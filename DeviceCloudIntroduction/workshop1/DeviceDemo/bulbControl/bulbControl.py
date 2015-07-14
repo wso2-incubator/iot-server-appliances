df559949636451d79490de2e01484ddcbf9ddcce
Ace <ruwany.ace@gmail.com>
2015-07-14 13:49:36 +0530

    Adding mode to python script

--- DeviceCloudIntroduction/workshop1/DeviceDemo/bulbControl/bulbControl.py
+++ DeviceCloudIntroduction/workshop1/DeviceDemo/bulbControl/bulbControl.py
@@ -14,6 +14,7 @@ def blink(pin):
 BULB_PIN = 11
 # to use Raspberry Pi board pin numbers
 # set up GPIO output channel
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(BULB_PIN, GPIO.OUT)
 
 # blink GPIO17 50 times
