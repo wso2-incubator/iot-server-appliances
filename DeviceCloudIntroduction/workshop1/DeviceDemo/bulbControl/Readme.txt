8ede099b480352995f2c5664df8141e430a61a17
Ace <ruwany.ace@gmail.com>
2015-07-14 13:00:17 +0530

    Adding device cloud demo code

---
+++ DeviceCloudIntroduction/workshop1/DeviceDemo/bulbControl/Readme.txt
@@ -0,0 +1,5 @@
+After running the *.deb file downloaded from the device cloud please copy the bulbControl.py to the raspberry pi and run it using the following command.
+
+sudo python lightControl.py
+
+In order to control the buzzer, change the BULB_PIN parameter to 12.
---
+++ DeviceCloudIntroduction/workshop1/DeviceDemo/bulbControl/bulbControl.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import time
+
+# blinking function
+def blink(pin):
+       GPIO.output(pin,True)
+       time.sleep(1)
+       GPIO.output(pin,False)
+       time.sleep(1)
+       return
+
+BULB_PIN = 11
+# to use Raspberry Pi board pin numbers
+# set up GPIO output channel
+GPIO.setup(BULB_PIN, GPIO.OUT)
+
+# blink GPIO17 50 times
+for i in range(0,50):
+       blink(BULB_PIN)
+
+GPIO.cleanup()
